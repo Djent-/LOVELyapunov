3255f601f0a943437c1b2e8ccb75a6aa58ad83ae
Djent- <inflames2361@aim.com>
2014-08-10 21:31:05 -0400

    Initial commit. Added main.lua and starter code

---
+++ main.lua
@@ -0,0 +1,22 @@
+--main.lua
+--based on the golfed code in the codegolf stackexchange
+
+function love.load()
+	DIM = 512 --1024 in the original
+end
+
+function love.draw()
+
+end
+
+function red(i, j)
+
+end
+
+function green(i, j)
+
+end
+
+function blue(i, j)
+
+end
