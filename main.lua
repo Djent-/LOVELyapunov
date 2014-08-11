51a63d23d319e1d26819a25db11f1920672157a1
Djent- <inflames2361@aim.com>
2014-08-10 22:12:44 -0400

    Draws colors now, not in any pattern though...

--- main.lua
+++ main.lua
@@ -1,18 +1,42 @@
 --main.lua
 --based on the golfed code in the codegolf stackexchange
 
+function love.conf(t)
+	t.console = true
+end
+
 function love.load()
+	math.randomseed(os.time())
+	math.random();math.random();
 	DIM = 512 --1024 in the original
-	canvas = love.graphics.newCanvas()
+	MAX_ITERATIONS = 1000
+	love.window.setMode(DIM,DIM)
+	canvas = love.graphics.newCanvas(DIM,DIM)
+	love.graphics.setCanvas(canvas)
+	local ymin, ymax, xmin, xmax = 2, 4, 2, 4
+	local increment = (ymax - ymin) / DIM
+	for y = ymin, ymax, increment do
+		for x = xmin, xmax, increment do
+			love.graphics.setColor(red(x,y), green(x,y), blue(x,y))
+			local _x = math.abs((x - xmin) / increment)
+			local _y = math.abs((y - ymin) / increment)
+			love.graphics.rectangle("fill",_x, _y,1,1)
+		end
+	end
+	love.graphics.setColor(255,255,255)
+	love.graphics.print("Finished drawing",10,10)
+	print("Finished drawing")
+	love.graphics.setCanvas()
 end
 
 function love.draw()
-	
+	love.graphics.draw(canvas,1,1)
+	love.graphics.print("Drawing",10,30)
 end
 
 function red(i, j)
 	local r, s, x = nil, 0, 0.5
-	for k = 0, 50 do
+	for k = 0, MAX_ITERATIONS do
 		if k % 5 == 2 or k % 5 == 4 then
 			r = (2 * j) / DIM + 2
 		else
@@ -26,7 +50,7 @@ end
 
 function green(i, j)
 	local r, s, x = nil, 0, 0.5
-	for k = 0, 50 do
+	for k = 0, MAX_ITERATIONS do
 		if k % 5 == 2 or k % 5 == 4 then
 			r = (2 * j) / DIM + 2
 		else
@@ -44,7 +68,7 @@ end
 
 function blue(i, j)
 	local r, s, x = nil, 0, 0.5
-	for k = 0, 50 do
+	for k = 0, MAX_ITERATIONS do
 		if k % 5 == 2 or k % 5 == 4 then
 			r = (2 * j) / DIM + 2
 		else
